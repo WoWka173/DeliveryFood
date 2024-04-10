0feb3a6fd9e091386d2f06cf6ce90fe50ea649ae
WoWka173 <107262465+WoWka173@users.noreply.github.com>
2024-04-10 18:58:10 +0400

    Added custom fonts

--- DeliveryFood/DeliveryFood/ViewController.swift
+++ DeliveryFood/DeliveryFood/ViewController.swift
@@ -12,6 +12,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
+        
+        let label = UILabel()
+        label.text = "Hello World"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        
+        label.font = .systemFont(ofSize: 40, weight: UIFont.Weight(20))
+        label.textColor = .white
     }
 
 
