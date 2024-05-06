9e8ba7db2200fd1539518a85d62cf6fd963e1b6e
WoWka173 <107262465+WoWka173@users.noreply.github.com>
2024-05-06 20:33:13 +0400

    Added Factory

--- DeliveryFood/DeliveryFood/NavigationLayer/Coordinators/OnboardingCoordinator.swift
+++ DeliveryFood/DeliveryFood/NavigationLayer/Coordinators/OnboardingCoordinator.swift
@@ -7,8 +7,13 @@
 
 import UIKit
 
+//MARK: - OnboardingCoordinator
 class OnboardingCoordinator: Coordinator {
     
+    //MARK: - Properties
+    private let factory = SceneFactory.self
+    
+    //MARK: - Methods
     override func start() {
         showOnboarding()
     }
@@ -19,41 +24,10 @@ class OnboardingCoordinator: Coordinator {
     }
 }
 
+//MARK: - Navigation
 private extension OnboardingCoordinator {
     func showOnboarding() {
-        var pages = [OnboardingPartViewController]()
-        
-        let firstVC = OnboardingPartViewController()
-        firstVC.imageToShow = UIImage(resource: .chicken)
-        firstVC.titleText = "Delicious Food"
-        firstVC.descriptionText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-        firstVC.buttonText = "Next1"
-        
-        let secondVC = OnboardingPartViewController()
-        secondVC.imageToShow = UIImage(resource: .shipped)
-        secondVC.titleText = "Fast Shipping"
-        secondVC.descriptionText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interdum rhoncus nulla."
-        secondVC.buttonText = "Next2"
-        
-        let thirdVC = OnboardingPartViewController()
-        thirdVC.imageToShow = UIImage(resource: .medal)
-        thirdVC.titleText = "Certificate Food"
-        thirdVC.descriptionText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ultricies mauris a id."
-        thirdVC.buttonText = "Next3"
-        
-        let fourthVC = OnboardingPartViewController()
-        fourthVC.imageToShow = UIImage(resource: .credit)
-        fourthVC.titleText = "Payment Online"
-        fourthVC.descriptionText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dui ultricies sit massa."
-        fourthVC.buttonText = "Next4"
-        
-        pages.append(firstVC)
-        pages.append(secondVC)
-        pages.append(thirdVC)
-        pages.append(fourthVC)
-        
-        let presenter = OnBoardingViewPresenter(coordinator: self)
-        let viewController = OnBoardingViewController(pages: pages, viewOutput: presenter)
+        let viewController = factory.createOnboardingScene(coordinator: self)
         navigationController?.pushViewController(viewController, animated: true)
     }
 }
